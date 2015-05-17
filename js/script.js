3ed7b8b97e9f0d6365e7d3a9ce84ee4a338562b6
Patrick Lühne <patrick.luehne@student.hpi.uni-potsdam.de>
2015-05-17 18:26:20 +0200

    Initial commit.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function()
+{
+	$("pre.cpp").snippet("cpp", {style: "whitengrey", transparent: true, showNum: false, menu: false});
+	$(".carousel").carousel({interval: false, wrap: false});
+});
